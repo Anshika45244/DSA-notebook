8b52df34a5e5c6c6651157b99c2c2bbf404bde38
Anshika Verma <anshikaverma6334@gmail.com>
2025-08-25 14:28:26 -0400

    Create 2d array.cpp

---
+++ 2d array.cpp
@@ -0,0 +1,332 @@
+/*#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][4];// creating a 2d array with 3 rows and 4 col
+    //input elements row_wise
+    for(int i=0; i<3;i++)
+    {
+        for(int j=0;j<4;j++){
+            cin>>arr[i][j];
+        }
+    }
+    //output print karalo
+       for(int i=0; i<3;i++)
+    {
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][4];// creating a 2d array with 3 rows and 4 col
+    //input elements col_wise
+    for(int i=0; i<4;i++)
+    {
+        for(int j=0;j<3;j++){
+            cin>>arr[j][i];
+        }
+    }
+    //output print karalo>>>>coln vise print kar rahe hai
+       for(int i=0; i<3;i++)
+    {
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+// how to do hard code
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};// creating a 2d array with 3 rows and 4 col
+
+    //output print karalo>
+       for(int i=0; i<3;i++)
+    {
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3][4]={{1,11,111,1111},{2,22,222,2222},{3,33,333,3333}};// creating a 2d array with 3 rows and 4 col
+
+    //output print karalo>
+       for(int i=0; i<3;i++)
+    {
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+#include<iostream>
+using namespace std;
+bool ispresent(int arr[][4],int target, int row, int col)
+{
+    for(int row =0; row<3;row++){
+        for(int col =0; col<4;col++){
+    if(arr[row][col]==target){
+        return 1;            
+        }
+    }
+}
+return 0;
+}
+int main(){
+    int arr[3][4];
+    //taking input>>>>>> row wise 
+    for(int row=0; row<3; row++){
+        for(int col =0; col<4; col++){
+            cin>>arr[row][col];
+        }
+        
+    }
+        
+    //taking input>>>>>> row wise 
+    for(int row=0; row<3; row++){
+        for(int col =0; col<4; col++){
+            cout<<arr[row][col];
+        }
+        cout<<endl;
+    }
+cout<<"enter the element to search "<<endl;
+int target ;
+cin>>target;
+if(ispresent(arr,target,3,4)){
+    cout<<"present"<<endl;
+    
+}
+else{
+    cout<<"absent"<<endl;
+}
+return 0;
+}
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
