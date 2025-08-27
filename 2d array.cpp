c3d5f4753266902a1703fb0a2581ffcfc6bca4a5
Anshika Verma <anshikaverma6334@gmail.com>
2025-08-27 09:42:44 -0400

    Update 2d array.cpp

--- 2d array.cpp
+++ 2d array.cpp
@@ -226,7 +226,37 @@ cout<<"the row with the max"<<index<<endl;
   }
 
 
-
+#include<iostream>
+#include<vector>
+using namespace std;
+bool searchMatrix(vector<vector<int>>&matrix,int target){
+    int row= matrix.size();
+    int col = matrix[0].size();
+    int start =0;
+    int end = row*col -1;
+    while(start<=end){
+        int mid = start+(end-start)/2;
+        int element = matrix[mid/col][mid%col];
+        if(element ==target)return true;
+        if(element <target )start=mid +1;
+        else end = mid -1;
+    }
+    return false;
+    
+}
+int main(){
+    vector<vector<int>>matrix={{1,2,3},
+    {5,4,6},{4,5,6}};
+    int target = 1;
+    if(searchMatrix,target){
+        cout<<"target found"<<endl;
+        
+    }
+    else{
+        cout<<"target not found"<<endl;
+    }
+    return 0;
+}
 
 
 */
