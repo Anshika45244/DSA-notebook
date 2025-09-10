cf375e6f108d6f2a68c8a381b3f6e604930e73d9
Anshika Verma <anshikaverma6334@gmail.com>
2025-09-10 12:52:29 -0400

    Create reference.cpp

---
+++ reference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int i = 5;
+    //create a ref variable 
+    int& j = i;
+   cout<<i<<endl;
+   i++;
+   cout<<j<<endl;
+   j++;
+   cout<<i<<endl;
+   cout<<j<<endl;
+    return 0;
+}
