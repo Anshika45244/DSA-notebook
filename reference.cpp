5c45e9b4f931fcd47d6002c7d081e98d372b6856
Anshika Verma <anshikaverma6334@gmail.com>
2025-09-11 14:20:19 -0400

    Update reference.cpp

--- reference.cpp
+++ reference.cpp
@@ -1,15 +1,38 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
-int main(){
-    
-    int i = 5;
-    //create a ref variable 
-    int& j = i;
-   cout<<i<<endl;
-   i++;
-   cout<<j<<endl;
-   j++;
-   cout<<i<<endl;
-   cout<<j<<endl;
-    return 0;
+/*void update(int n){
+    n++;*/
+ /*   void update(int& n){
+    n++;*/
+  /*  int& update(int m)
+    {
+        int a = 10;
+         int& ans = a;
+         return ans;*///kabhi nhi kARNA chahiye
+        
+  /*  }
+  int* fun(int n){
+      
+int*ptr = &n;
+return ptr;
 }
+ int main(){
+     
+     /*int i = 5;
+      int& j= i;
+      cout<<i<<endl;
+      cout<<j<<endl;
+      i++;
+      cout<<j<<endl;
+      j++;
+      cout<<j<<endl;\
+      int n= 5;
+  /*    cout<<"before"<<n<<endl;
+      update(n);
+      cout<<"after"<<n<<endl;
+      */
+    /*  fun(n); ////kabhi nhi kARNA chahiye
+      return 0;
+ }*/
+ 
+ 
